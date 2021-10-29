dfdfe86ac3ab34b428cc7625a590398d7ced8388
m1guri <ejann.alvin@gmail.com>
2021-10-29 10:54:42 +0800

    first commit

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+
+</div>
+<!-- footer -->
+<footer class="footer-container row justify-content-center mt-4 py-3">
+	<div class="text-white">Aking's Sari-sari by Jann Alvin & Samboy</div>
+</footer>
+<script src="<?php echo $server; ?>js/script.js"></script>
+</body>
+</html>
